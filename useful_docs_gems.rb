1f4df7f1bf8a533db9c7ff8238841aad2caa7c1a
Rafael Simão <82464125+rafaelcosta-hotmart@users.noreply.github.com>
2025-06-03 14:42:22 -0300

    Update and rename useful_gems.rb to useful_docs_gems.rb

---
+++ useful_docs_gems.rb
@@ -0,0 +1,10 @@
+# List of Useful Gems
+
+### Docs
+- [Investigating the Performance of a Problematic Rails API Endpoint](https://dev.to/mculp/investigating-the-performance-of-a-problematic-rails-api-endpoint-3a65)
+
+### Gems
+
+#### Gem Licenses
+A gem used to document the gems with versions and licenses.
+Gem [link](https://github.com/dblock/gem-licenses/)
--- useful_gems.rb
+++
@@ -1,7 +0,0 @@
-# List of Useful Gems
-
-### Gem Licenses
-
-A gem used to document the gems with versions and licenses.
-
-Gem [link](https://github.com/dblock/gem-licenses/)
